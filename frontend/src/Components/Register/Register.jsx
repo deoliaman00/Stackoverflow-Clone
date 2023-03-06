bc0619c32daa9b026721aeda0ee070650afe95a1
deoliaman00 <deoliaman00@gmail.com>
2023-03-06 20:58:57 +0530

    done with the register feature

--- frontend/src/Components/Register/Register.jsx
+++ frontend/src/Components/Register/Register.jsx
@@ -1,19 +1,39 @@
 import React, { useState } from "react";
 import { Container, Form, Button } from "react-bootstrap";
 import "./Register.css";
-
+import axios from "axios";
 function Register() {
-  const [firstName, setFirstName] = useState("");
-  const [lastName, setLastName] = useState("");
+  const [first_name, setfirst_name] = useState("");
+  const [last_name, setlast_name] = useState("");
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+    const [errorMessage, setErrorMessage] = useState("");
 
-  const handleFirstNameChange = (event) => {
-    setFirstName(event.target.value);
+  const HandleSubmit = (event) => {
+    event.preventDefault();
+    const data = {
+      first_name: first_name,
+      last_name: last_name,
+      email: email,
+      password: password,
+    };
+    axios
+      .post("http://127.0.0.1:8000/api/register/", data)
+      .then((response) => {
+        console.log(response.data);
+        window.open("http://localhost:3000/login", "_self");
+      })
+      .catch((error) => {
+        console.error(error);
+        setErrorMessage("Email already exists");
+      });
+  };
+  const handlefirst_nameChange = (event) => {
+    setfirst_name(event.target.value);
   };
 
-  const handleLastNameChange = (event) => {
-    setLastName(event.target.value);
+  const handlelast_nameChange = (event) => {
+    setlast_name(event.target.value);
   };
 
   const handleEmailChange = (event) => {
@@ -26,20 +46,21 @@ function Register() {
 
   const handleSubmit = (event) => {
     event.preventDefault();
-    console.log("Submitted:", { firstName, lastName, email, password });
+    console.log("Submitted:", { first_name, last_name, email, password });
   };
 
   return (
     <Container className="d-flex justify-content-center align-items-center container-size bg-light-blue mt-4 ">
       <Form onSubmit={handleSubmit}>
         <h1 className="mb-4 display-1 font-weight-bold">Register</h1>
+        {errorMessage && <p className="text-danger">{errorMessage}</p>}
         <Form.Group>
           <Form.Label>First name</Form.Label>
           <Form.Control
             type="text"
             placeholder="Enter your first name"
-            value={firstName}
-            onChange={handleFirstNameChange}
+            value={first_name}
+            onChange={handlefirst_nameChange}
           />
         </Form.Group>
         <Form.Group>
@@ -47,8 +68,8 @@ function Register() {
           <Form.Control
             type="text"
             placeholder="Enter your last name"
-            value={lastName}
-            onChange={handleLastNameChange}
+            value={last_name}
+            onChange={handlelast_nameChange}
           />
         </Form.Group>
         <Form.Group>
@@ -69,7 +90,12 @@ function Register() {
             onChange={handlePasswordChange}
           />
         </Form.Group>
-        <Button className="mt-3" variant="primary" type="submit">
+        <Button
+          className="mt-3"
+          variant="primary"
+          type="submit"
+          onClick={HandleSubmit}
+        >
           Submit
         </Button>
       </Form>
